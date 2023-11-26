0f0c489dfc3b064c3e28ffd37603f46fe1d005aa
Stephanie Marie <fullstackwebdevsteph@gmail.com>
2023-11-26 18:00:16 -0400

    figured out how to create and added a timer. Added one sample question/answers to see if I got it all right and in working order

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,44 @@
-/* Things to remember:
-1. Place all variables at top of js file. 
+// keep ALL variables at the top
 
+var grabStartBtn = document.getElementById('startBtn'); //creates  reference to HTML button with id 'startBtn'
+/*Timer variables*/
+var timer; //To store the interval ID
+var timeRemaining = 120; //time in seconds
+var questions = [];
+questions.push({
+  //question
+  question: 'What main coding language are we learning?',
+  //Answers array
+  answers: [
+    { text: 'Python', correct: false },
+    { text: 'Java', correct: false },
+    { text: 'JavaScript', correct: true },
+  ],
+});
 
-/* 1. var startButton is setting a variable to select the button to work with*/
-var startBtn = document.getElementById('startBtn');
-/*crazy func name but usefull as I learn and for when I revisit code */
-function clickedToStartTimerPresentQuestion() {
-  alert('OMG! This worked!!');
-  //need to present&start timer
-  //need to present quetsion
+/*Start timer function */
+function startTimer() {
+  //Sets up an interval to update timer every second
+  timer = setInterval(function () {
+    // Display the time remaining on the screen
+    document.getElementById('timerDisplay').textContent =
+      'Time: ' + timeRemaining + 's';
+
+    /*checking if time ran out */
+    if (timeRemaining <= 0) {
+      clearInterval(timer); //stops timer
+      alert('Times up!');
+    }
+    timeRemaining--;
+  }, 1000); //updates time every 1 second
 }
 
-startBtn.addEventListener('click', clickedToStartTimerPresentQuestion);
+// function thats triggered when button is clicked
+function clickedToStartTimerPresentQuestion() {
+  //alert(' This worked!!');
+  startTimer(); //start the timer to begin countdown
+
+  //need to present quetsion
+}
+//attached eventListener to button to trigger function
+grabStartBtn.addEventListener('click', clickedToStartTimerPresentQuestion);
